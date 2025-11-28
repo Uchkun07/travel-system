00f470808bdd639c2f6cfd2e5f537ed71b7ad530
PG_Mirz <mirz.mamateli@gmail.com>
2025-11-28 11:18:41 +0800

    fix(backend): 日志管理出现错误

--- backend/springProject/src/main/java/io/github/uchkun07/travelsystem/SpringProjectApplication.java
+++ backend/springProject/src/main/java/io/github/uchkun07/travelsystem/SpringProjectApplication.java
@@ -2,8 +2,10 @@ package io.github.uchkun07.travelsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 public class SpringProjectApplication {
 
 	public static void main(String[] args) {
